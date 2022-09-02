1f8f47956ac7a30c70535765354d0e55e235c11a
Vladimir Borisov <1mt-2@mail.ru>
2022-09-02 17:57:39 +0300

    added custom select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, KeyboardEvent, useEffect} from "react";
 import s from './Select.module.css'
 
 type ItemType = {
@@ -15,25 +15,62 @@ type SelectPropsType = {
 export function Select(props: SelectPropsType) {
 
     const [active, setActive] = useState(false)
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
 
     const selectItem = props.items.find(i => i.value === props.value);
+    const hoveredElement = props.items.find(i => i.value === hoveredElementValue)
+
+    useEffect(() => {
+        setHoveredElementValue(props.value)
+    }, [props.value])
 
     const showItems = () => setActive(!active)
 
-    return (<>
+    const onItemClick = (value: any) => {
+        props.onChange(value);
+        showItems()
+    }
+
+    const onkeyup = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElementValue) {
+                    const nextpretElement = e.key === 'ArrowDown' ? props.items[i+1] : props.items[i-1]
+                    if (nextpretElement) {
+                        props.onChange(nextpretElement.value)
+                        return;
+                    }
+                }
+            }
+            if(!selectItem){
+            props.onChange(props.items[0].value)}
+        }
+
+        if (e.key === 'Enter' || e.key === 'Escape') {
+            setActive(false)
+        }
+    }
+
+
+    return (
+        <>
             <select value={'2'}>
                 <option value="1">Minsk</option>
                 <option value="2">Moscow</option>
                 <option value="3">Samara</option>
             </select>
 
-            <div className={s.select + ' ' + (active ? s.active : '')}>
+            <div className={s.select + ' ' + (active ? s.active : '')} onKeyUp={onkeyup} tabIndex={0}>
 
                 <span className={s.main} onClick={showItems}>{selectItem && selectItem.title}</span>
 
                 {active &&
-                    <div className={s.items}>
-                        {props.items.map(el => <div key={el.value}>{el.title}</div>)}
+                    <div className={s.items} >
+                        {props.items.map(el => <div
+                            onMouseEnter={() => setHoveredElementValue(el.value)}
+                            className={s.item + ' ' + (hoveredElement === el ? s.selected : '')}
+                            key={el.value}
+                            onClick={() => onItemClick(el.value)}>{el.title}</div>)}
                     </div>}
             </div>
         </>
--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,5 +1,5 @@
 import {action} from "@storybook/addon-actions";
-import React from "react";
+import React, {useState} from "react";
 import {Select} from "../components/Select/Select";
 
 
@@ -8,21 +8,29 @@ export default {
     component: Select,
 };
 
-export const SelectExample = () =>
-    <>
-        <Select value={'2'} onChange={action('Value changed')}
+export const SelectExample = () => {
+    const [value, setValue] = useState('2')
+
+    return <>
+        <Select value={value}
+                onChange={setValue}
                 items={[
                     {value: '1', title: 'Minsk'},
                     {value: '2', title: 'Moscow'},
                     {value: '3', title: 'Samara'}]}/>
 
     </>
+}
 
-export const SelectExampleWithoutValue = () => <>
+export const SelectExampleWithoutValue = () => {
+    const [value, setValue] = useState(null)
+    return <>
 
-    <Select onChange={action('Value changed')}
-            items={[
-                {value: '1', title: 'Minsk'},
-                {value: '2', title: 'Moscow'},
-                {value: '3', title: 'Samara'}]}/>
-</>
+        <Select onChange={setValue}
+                value={value}
+                items={[
+                    {value: '1', title: 'Minsk'},
+                    {value: '2', title: 'Moscow'},
+                    {value: '3', title: 'Samara'}]}/>
+    </>
+}
